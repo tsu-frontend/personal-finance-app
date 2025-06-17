1f87b002fc49fd0e6eac5a4a5f4a6220cf6fe169
alsandre <gabeditebuli@gmail.com>
2025-06-17 11:12:13 +0400

    wip

--- scripts/api/deleteFetch.js
+++ scripts/api/deleteFetch.js
@@ -1,7 +1,14 @@
+import { ServiceManager } from "supabase-service-manager";
 import { renderPotsData } from "../pots.js";
 // import { renderBudgetsData } from "../budgets.js";
 import { pageType } from "../utilities/pageType.js";
-
+const config = {
+  url: "https://dhpewqtvbasnugkfiixs.supabase.co",
+  anonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRocGV3cXR2YmFzbnVna2ZpaXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NzY1MzMsImV4cCI6MjA2MjQ1MjUzM30.8tYLfww-2KjIRsmJvCTQ1vBd3ghf0c4QNmW6TwPYVTk",
+};
+const sm = new ServiceManager({ supabase: config });
+console.log(sm);
 const SUPABASE_URL = `https://dhpewqtvbasnugkfiixs.supabase.co`;
 const PUBLIC_KEY = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRocGV3cXR2YmFzbnVna2ZpaXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NzY1MzMsImV4cCI6MjA2MjQ1MjUzM30.8tYLfww-2KjIRsmJvCTQ1vBd3ghf0c4QNmW6TwPYVTk`;
 
