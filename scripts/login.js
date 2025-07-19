0ad2bec24805f75f1f00b5131d61ac8d00fca7da
natia <natia.karamnishvili369@law.tsu.ge>
2025-07-19 16:42:23 +0400

    wip

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,14 @@
-class SignUpForm {
+import { FormUtility } from "./utilities/formUtility.js";
+class SignInForm {
     constructor(element) {
         this.formElement = element;
         this.inputElementsList = this.formElement.elements;
         this.valid = false;
         this.data = {
-            name: { value: null, isValid: false },
             email: { value: null, isValid: false },
             password: { value: null, isValid: false },
         };
+        this.formUtility = new FormUtility();
         this.setUpListeners();
     }
     setUpListeners() {
@@ -15,55 +16,50 @@ class SignUpForm {
             if (element instanceof HTMLButtonElement) {
                 element.addEventListener("click", (e) => {
                     e.preventDefault();
-                    //
-                    console.log("clicked");
+                    console.log(e);
                 });
             }
-            else {
+            else if (element instanceof HTMLInputElement) {
                 element.addEventListener("change", (e) => {
-                    const target = e.target;
-                    //
-                    console.log("change");
+                    // console.log(element.value)
+                    this.validateInputValue(element);
                 });
             }
         });
     }
+    validateInputValue(target) {
+        if (target.type === "email") {
+            const isEmailValid = this.formUtility.emailValidator(target.value);
+            this.data.email.value = target.value;
+            this.data.email.isValid = isEmailValid;
+            console.log("dsd");
+        }
+        if (target.type === "password") {
+            const isPasswordValid = this.formUtility.passwordValidator(target.value);
+            this.data.password.value = target.value;
+            this.data.password.isValid = isPasswordValid;
+        }
+        if (target.type === "name") {
+            if (target.name === "name") {
+                const isNameValid = this.formUtility.nameValidator(target.value);
+                this.data.name.value = target.value;
+                this.data.name.isValid = isNameValid;
+            }
+        }
+    }
+    async submitFormData(formData) { }
 }
-const signUpForm = document.getElementById("signup-form");
-const newSignUpForm = new SignUpForm(signUpForm);
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
-class SignInForm extends SignUpForm {
+class SignUpForm extends SignInForm {
     constructor(element) {
         super(element);
-        this.formElement = element;
-        this.inputElementsList = this.formElement.elements;
-        this.valid = false;
-        this.data = {
-            email: { value: null, isValid: false },
-            password: { value: null, isValid: false },
-        };
-        this.setUpListeners();
-    }
-    setUpListeners() {
-        [...this.inputElementsList].forEach((element) => {
-            if (element instanceof HTMLButtonElement) {
-                element.addEventListener("click", (e) => {
-                    e.preventDefault();
-                    //
-                    console.log("clicked");
-                });
-            }
-            else {
-                element.addEventListener("change", (e) => {
-                    const target = e.target;
-                    //
-                    console.log("change");
-                });
-            }
-        });
+        this.data = Object.assign(Object.assign({}, this.data), { name: { value: null, isValid: false } });
     }
 }
 const signInForm = document.getElementById("signin-form");
 const newSignInForm = new SignInForm(signInForm);
+// console.log(newSignInForm);
+const signUpForm = document.getElementById("signup-form");
+const newSignUpForm = new SignUpForm(signUpForm);
--- scripts/login.ts
+++ scripts/login.ts
@@ -1,15 +1,20 @@
-type TFormData = {
-  name?: { value: null | string; isValid: boolean };
+import { FormUtility } from "./utilities/formUtility.js";
+
+interface TSignIn {
   email: { value: null | string; isValid: boolean };
   password: { value: null | string; isValid: boolean };
-};
+}
+interface TSignUp extends TSignIn {
+  name: { value: null | string; isValid: boolean };
+}
 
-class SignUpForm {
-  data: TFormData;
+class SignInForm {
+  data: TSignIn;
 
   public formElement: HTMLFormElement;
   public inputElementsList: HTMLFormControlsCollection;
   public valid: boolean;
+  public formUtility: FormUtility;
 
   constructor(element: HTMLFormElement) {
     this.formElement = element;
@@ -17,10 +22,11 @@ class SignUpForm {
     this.valid = false;
 
     this.data = {
-      name: { value: null, isValid: false },
       email: { value: null, isValid: false },
       password: { value: null, isValid: false },
     };
+
+    this.formUtility = new FormUtility();
 
     this.setUpListeners();
   }
@@ -30,65 +36,62 @@ class SignUpForm {
       if (element instanceof HTMLButtonElement) {
         element.addEventListener("click", (e: MouseEvent) => {
           e.preventDefault();
-          //
-          console.log("clicked");
+          console.log(e);
         });
-      } else {
-        element.addEventListener("change", (e) => {
-          const target = e.target;
-          //
-          console.log("change");
+      } else if (element instanceof HTMLInputElement) {
+        element.addEventListener("change", (e: Event) => {
+          // console.log(element.value)
+
+          this.validateInputValue(element);
         });
       }
     });
   }
+  validateInputValue(target: HTMLInputElement) {
+    if (target.type === "email") {
+      const isEmailValid = this.formUtility.emailValidator(target.value);
+      this.data.email.value = target.value;
+      this.data.email.isValid = isEmailValid;
+      console.log("dsd");
+    }
+    if (target.type === "password") {
+      const isPasswordValid = this.formUtility.passwordValidator(target.value);
+      this.data.password.value = target.value;
+      this.data.password.isValid = isPasswordValid;
+    }
+    if (target.type === "name") {
+      if (target.name === "name") {
+        const isNameValid = this.formUtility.nameValidator(target.value);
+        this.data.name.value = target.value;
+        this.data.name.isValid = isNameValid;
+      }
+    }
+  }
+
+  async submitFormData(formData: TSignIn) {}
 }
-const signUpForm = document.getElementById("signup-form");
-const newSignUpForm = new SignUpForm(signUpForm as HTMLFormElement);
 
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 
-class SignInForm extends SignUpForm {
-  data: TFormData;
-
-  public formElement: HTMLFormElement;
-  public inputElementsList: HTMLFormControlsCollection;
-  public valid: boolean;
+class SignUpForm extends SignInForm {
+  data: TSignUp;
 
   constructor(element: HTMLFormElement) {
     super(element);
-    this.formElement = element;
-    this.inputElementsList = this.formElement.elements;
-    this.valid = false;
 
     this.data = {
-      email: { value: null, isValid: false },
-      password: { value: null, isValid: false },
+      ...this.data,
+      name: { value: null, isValid: false },
     };
-
-    this.setUpListeners();
   }
 
-  setUpListeners() {
-    [...this.inputElementsList].forEach((element) => {
-      if (element instanceof HTMLButtonElement) {
-        element.addEventListener("click", (e: MouseEvent) => {
-          e.preventDefault();
-          //
-          console.log("clicked");
-        });
-      } else {
-        element.addEventListener("change", (e) => {
-          const target = e.target;
-          //
-          console.log("change");
-        });
-      }
-    });
-  }
 }
 
 const signInForm = document.getElementById("signin-form");
 const newSignInForm = new SignInForm(signInForm as HTMLFormElement);
+// console.log(newSignInForm);
+
+const signUpForm = document.getElementById("signup-form");
+const newSignUpForm = new SignUpForm(signUpForm as HTMLFormElement);
